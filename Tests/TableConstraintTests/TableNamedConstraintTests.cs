8c1f87a7a4fe0a0bead4a734d987fc55faa3755a
Daniel Hughes <trampster@gmail.com>
2023-08-19 20:14:58 +1200

    Support named table constraints (#21)

---
+++ Tests/TableConstraintTests/TableNamedConstraintTests.cs
@@ -0,0 +1,28 @@
+using Moq;
+using SqlSrcGen;
+
+namespace Tests.TableConstraintTests;
+
+public class TableNamedConstraintTests
+{
+    [Test]
+    public void TableNamedConstraint_Valid_CreatesTableInfo()
+    {
+        // arrange
+        var generator = new SqlGenerator();
+        var databaseInfo = new DatabaseInfo();
+
+        // act
+        generator.ProcessSqlSchema($"CREATE TABLE contact (name Text, constraint ConsraintName unique (name));", databaseInfo, Mock.Of<IDiagnosticsReporter>());
+
+        // assert
+        Assert.That(databaseInfo.Tables[0].SqlName, Is.EqualTo("contact"));
+        Assert.That(databaseInfo.Tables[0].CSharpName, Is.EqualTo("Contact"));
+        Assert.That(databaseInfo.Tables[0].Columns[0].SqlName, Is.EqualTo("name"));
+        Assert.That(databaseInfo.Tables[0].Columns[0].CSharpName, Is.EqualTo("Name"));
+        Assert.That(databaseInfo.Tables[0].Columns[0].SqlType, Is.EqualTo("Text"));
+        Assert.That(databaseInfo.Tables[0].Columns[0].CSharpType, Is.EqualTo("string?"));
+        Assert.That(databaseInfo.Tables[0].Columns[0].TypeAffinity, Is.EqualTo(TypeAffinity.TEXT));
+        Assert.That(databaseInfo.Tables[0].Columns[0].Unique, Is.True);
+    }
+}
